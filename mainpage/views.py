62326edf26737ecd7050aa687c0128973852849e
Vladimir1781982 <worker777@inbox.ru>
2015-06-02 00:22:14 +0300

    Step1

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.utils import timezone
+from datetime import datetime, date, time
+#from django.template.defaultfilters import slugify
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def index(request):
+    return render(request, 'mainpage/index.html')
